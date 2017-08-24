dc9bc182b4c6425139b0e1ac604424c963cefe81
Gerardo-Pastor <31077829+Gerardo-Pastor@users.noreply.github.com>
2017-08-24 12:16:25 -0500

    Add files via upload
    Detecta el numero de seria de la USB y puede desmontarla, siempre pregunta no detecta

--- firewall.sh
+++ firewall.sh
@@ -1,1 +1,18 @@
-#!bin/bash 
+#!/bin/bash
+
+while [ true ]; do
+	detect=dmesg | tail -n20 | grep -s "USB Mass Storage device detected"
+	echo $detect
+	if [ -n $detect ]; then
+		USBSERIAL=$(dmesg | tail -n20 | grep ": Serial" | awk '{print $6}' )
+		echo "USB with serialnumber: $USBSERIAL detected select a choice"
+		echo "1) Eject inmidiatly  2) Let it be  3) Add to whitelist"
+		read opcion
+		case $opcion in
+			1) eject /dev/sdc1;;
+			2) echo "doing nothing";;
+			3) echo $USBSERIAL >> whitelist.txt;;
+			*) echo "Number not valid";;
+		esac
+	fi
+done
